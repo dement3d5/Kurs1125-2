51329139dd8f6d66c021d9c0bb8f6e4c6ec14f21
Нурлан Аминов <alimovn125@gmail.com>
2022-05-01 20:40:04 +1000

    что-то добавил

--- Kurs1125/Model/MySqlDB.cs
+++ Kurs1125/Model/MySqlDB.cs
@@ -1,1 +1,116 @@
-﻿
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurs1125
+{
+    public class MySqlDB
+    {
+        private MySqlDB() { }
+        static MySqlDB db;
+        public static MySqlDB GetDB()
+        {
+            if (db == null)
+                db = new MySqlDB();
+            return db;
+        }
+
+
+        public MySqlConnection
+                GetDBConnection()
+        {
+            InitConnection();
+
+            return conn;
+        }
+        public bool OpenConnection()
+        {
+            try
+            {
+                if (conn == null)
+                    InitConnection();
+                conn.Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                System.Windows.MessageBox.Show(e.Message);
+            }
+            return false;
+        }
+
+        public void CloseConnection()
+        {
+            try
+            {
+                conn.Close(); // закрытие соединения
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        internal MySqlConnection conn = null;
+        internal void InitConnection()
+        {
+            InitConnection(Properties.Settings.Default.server, Properties.Settings.Default.user,
+                Properties.Settings.Default.db);
+        }
+        internal void InitConnection(string host, string username, string database)
+        {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.UserID = username;
+            builder.Database = database;
+            builder.Server = host;
+            builder.CharacterSet = "utf8";
+            builder.ConnectionTimeout = 5;
+
+            conn = new MySqlConnection(builder.GetConnectionString(true));
+        }
+
+
+        private int GetTableInfo(string table, string column)
+        {
+            int result = 0;
+            //SHOW TABLE STATUS WHERE `Name` = 'group'
+            if (OpenConnection())
+            {
+                using (MySqlCommand mc = new MySqlCommand($"SHOW TABLE STATUS WHERE `Name` = '{table}'", conn))
+                using (MySqlDataReader dr = mc.ExecuteReader())
+                {
+                    dr.Read();
+                    result = dr.GetInt32(column);
+                }
+                CloseConnection();
+            }
+            return result;
+        }
+        internal void ExecuteNonQuery(string query, MySqlParameter[] parameters = null)
+        {
+            if (OpenConnection())
+            {
+                using (MySqlCommand mc = new MySqlCommand(query, conn))
+                {
+                    if (parameters != null)
+                        mc.Parameters.AddRange(parameters);
+                    mc.ExecuteNonQuery();
+                }
+                CloseConnection();
+            }
+        }
+
+        internal int GetNextID(string table)
+        {
+            string column = "Auto_increment";
+            return GetTableInfo(table, column);
+        }
+        internal int GetRowsCount(string table)
+        {
+            string column = "Rows";
+            return GetTableInfo(table, column);
+        }
+    }
+}
